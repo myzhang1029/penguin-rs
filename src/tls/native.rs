341dc934750c1f8eb1ec2e99672ee07e831fde2b
Zhang Maiyun <me@maiyun.me>
2024-01-11 15:08:08 -0800

    Enable TLS test for all platforms
    Signed-off-by: Zhang Maiyun <me@maiyun.me>

--- src/tls/native.rs
+++ src/tls/native.rs
@@ -48,7 +48,7 @@ async fn read_key_cert(key_path: &str, cert_path: &str) -> Result<Identity, Erro
 
 #[cfg(test)]
 mod test {
-    // macOS and Windows don't support Ed25519 nor ECDSA-based certificates.
+    // `native_tls` on macOS and Windows doesn't support reading Ed25519 nor ECDSA-based certificates.
     #[tokio::test]
     #[cfg(not(any(target_os = "macos", target_os = "windows")))]
     async fn test_read_key_cert() {
