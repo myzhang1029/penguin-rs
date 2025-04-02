e2785a5a95b552eb7906291114805f2d43a3b6dd
Zhang Maiyun <me@maiyun.me>
2025-04-02 07:29:14 -0700

    Setup test deps

--- .github/workflows/pebble.sh
+++ .github/workflows/pebble.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+kill $(lsof -t -i :5002)
+kill $(lsof -t -i :14000)
+kill $(lsof -t -i :15000)
 git clone https://github.com/letsencrypt/pebble
 cd pebble && go install ./cmd/pebble || exit 1
 pebble &
