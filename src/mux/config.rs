dd244bb63a8c1b985e9bf95bf74f8e2f1c5be35a
Zhang Maiyun <me@maiyun.me>
2025-04-05 15:25:10 -0700

    Fix docs
    Signed-off-by: Zhang Maiyun <me@maiyun.me>

--- src/mux/config.rs
+++ src/mux/config.rs
@@ -10,21 +10,19 @@ pub const DATAGRAM_BUFFER_SIZE: usize = 1 << 9;
 /// need to have a crazy high buffer size.
 pub const STREAM_BUFFER_SIZE: usize = 1 << 5;
 
-/// Needs to be the same as `STREAM_FRAME_BUFFER_SIZE` but as `u64`
-pub const RWND: u64 = RWND_USIZE as u64;
+/// Needs to be the same as `RWND` but as `usize`
+pub const RWND_USIZE: usize = RWND as usize;
 
 /// Number of `StreamFrame`s to buffer in `MuxStream`'s channels before blocking
 #[cfg(not(test))]
-pub const RWND_USIZE: usize = 1 << 9;
+pub const RWND: u64 = 1 << 8;
+#[cfg(test)]
+pub const RWND: u64 = 4;
 /// Number of `Psh` frames between `Ack`s:
 /// If too low, `Ack`s will consume too much bandwidth;
 /// If too high, writers may block.
 #[cfg(not(test))]
 pub const DEFAULT_RWND_THRESHOLD: u64 = 1 << 8;
-
-/// Number of `StreamFrame`s to buffer in `MuxStream`'s channels before blocking
-#[cfg(test)]
-pub const RWND_USIZE: usize = 1 << 2;
 /// Number of `Psh` frames between `Ack`s. In tests, we want to be able to
 /// test the `Ack` mechanism, so we set this to be the same as the buffer size.
 /// The downside is that tests will be slower.
