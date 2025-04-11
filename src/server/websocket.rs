f2ce0ea1575fcc835a953bfb8cd7837d9fa28fc2
Zhang Maiyun <me@maiyun.me>
2025-04-10 20:53:35 -0700

    Server arg parsing
    Signed-off-by: Zhang Maiyun <me@maiyun.me>

--- src/server/websocket.rs
+++ src/server/websocket.rs
@@ -14,8 +14,8 @@ pub use penguin_mux::MuxStream;
 
 /// Multiplex the `WebSocket` connection and handle the forwarding requests.
 #[tracing::instrument(skip(ws_stream), level = "debug")]
-pub async fn handle_websocket(ws_stream: WebSocket) {
-    let mux = Multiplexor::new(ws_stream, OptionalDuration::NONE, false, None);
+pub async fn handle_websocket(ws_stream: WebSocket, reverse: bool) {
+    let mux = Multiplexor::new(ws_stream, OptionalDuration::NONE, reverse, None);
     debug!("WebSocket connection established");
     let mut jobs = JoinSet::new();
     // Channel for listeners to send UDP datagrams to the main loop
