cc8b84bf536c95d350345c82b492603b96f19491
Zhang Maiyun <me@maiyun.me>
2025-04-22 06:19:50 -0700

    Finish bench for stream
    Signed-off-by: Zhang Maiyun <me@maiyun.me>

--- benches/stream_throughput.rs
+++ benches/stream_throughput.rs
@@ -1,12 +1,22 @@
 use bytes::Bytes;
-use divan::Bencher;
-use penguin_mux::Dupe;
+use divan::{Bencher, counter::BytesCount};
+use penguin_mux::{Dupe, Multiplexor, timing::OptionalDuration};
 use std::sync::LazyLock;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
     runtime,
 };
+use tokio_tungstenite::{WebSocketStream, tungstenite::protocol::Role};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+
+fn setup_logging() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .try_init()
+        .ok();
+}
 
 static TOKIO_RT: LazyLock<runtime::Runtime> = LazyLock::new(|| {
     runtime::Builder::new_multi_thread()
@@ -22,56 +32,90 @@ fn make_payload() -> Bytes {
 }
 
 #[divan::bench]
-fn baseline(b: Bencher) {
-    b.with_inputs(make_payload)
-    .bench_values(|_| {
+fn baseline_ws(b: Bencher<'_, '_>) {
+    b.with_inputs(make_payload).bench_values(|_| {
         TOKIO_RT.block_on(async {
             let s_socket = TcpListener::bind(("::1", 0)).await.unwrap();
             let s_addr = s_socket.local_addr().unwrap();
             tokio::spawn(async move {
-                let mut s_stream = s_socket.accept().await.unwrap().0;
-                s_stream.shutdown().await.unwrap();
+                let tcpstream = s_socket.accept().await.unwrap().0;
+                let server = WebSocketStream::from_raw_socket(tcpstream, Role::Server, None).await;
+                let mux = Multiplexor::new(server, OptionalDuration::NONE, false, None);
+                let mut stream = mux.accept_stream_channel().await.unwrap();
+                stream.shutdown().await.unwrap();
             });
-            let mut c_stream = TcpStream::connect(s_addr).await.unwrap();
-            c_stream.shutdown().await.unwrap();
+            let tcpstream = TcpStream::connect(s_addr).await.unwrap();
+            let client = WebSocketStream::from_raw_socket(tcpstream, Role::Client, None).await;
+            let mux = Multiplexor::new(client, OptionalDuration::NONE, false, None);
+            let mut stream = mux.new_stream_channel(&[], 0).await.unwrap();
+            stream.shutdown().await.unwrap();
         });
     });
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128, 256])]
-fn stream_throughput(b: Bencher, num_writes: usize) {
+#[divan::bench(args = [1, 8, 64, 512, 4096, 32768])]
+fn baseline_tcp(b: Bencher<'_, '_>, num_writes: usize) {
     b.with_inputs(make_payload)
-    .bench_values(|payload| {
-        TOKIO_RT.block_on(async {
-            let s_socket = TcpListener::bind(("::1", 0)).await.unwrap();
-            let s_addr = s_socket.local_addr().unwrap();
-            let s_payload = payload.dupe();
-            tokio::spawn(async move {
-                let mut s_stream = s_socket.accept().await.unwrap().0;
+        .counter(BytesCount::new(num_writes * EACH_WRITE_SIZE))
+        .bench_values(|payload| {
+            TOKIO_RT.block_on(async {
+                let s_socket = TcpListener::bind(("::1", 0)).await.unwrap();
+                let s_addr = s_socket.local_addr().unwrap();
+                let s_payload = payload.dupe();
+                tokio::spawn(async move {
+                    let mut stream = s_socket.accept().await.unwrap().0;
+                    for _ in 0..num_writes {
+                        stream.write_all(&s_payload).await.unwrap();
+                    }
+                    stream.shutdown().await.unwrap();
+                });
+                let mut stream = TcpStream::connect(s_addr).await.unwrap();
+                stream.shutdown().await.unwrap();
+                let mut buf = vec![0; payload.len()];
                 for _ in 0..num_writes {
-                    s_stream.write_all(&s_payload).await.unwrap();
+                    stream.read_exact(&mut buf).await.unwrap();
                 }
-                s_stream.shutdown().await.unwrap();
-                let mut buf = vec![0; s_payload.len()];
-                for _ in 0..num_writes {
-                    s_stream.read_exact(&mut buf).await.unwrap();
-                }
-                assert_eq!(buf, s_payload);
+                assert_eq!(buf, payload);
             });
-            let mut c_stream = TcpStream::connect(s_addr).await.unwrap();
-            for _ in 0..num_writes {
-                c_stream.write_all(&payload).await.unwrap();
-            }
-            c_stream.shutdown().await.unwrap();
-            let mut buf = vec![0; payload.len()];
-            for _ in 0..num_writes {
-                c_stream.read_exact(&mut buf).await.unwrap();
-            }
-            assert_eq!(buf, payload);
         });
-    });
+}
+
+#[divan::bench(args = [1, 8, 64, 512, 4096, 32768])]
+fn bench_stream_throughput(b: Bencher<'_, '_>, num_writes: usize) {
+    b.with_inputs(make_payload)
+        .counter(BytesCount::new(num_writes * EACH_WRITE_SIZE))
+        .bench_values(|payload| {
+            TOKIO_RT.block_on(async {
+                let s_socket = TcpListener::bind(("::1", 0)).await.unwrap();
+                let s_addr = s_socket.local_addr().unwrap();
+                let s_payload = payload.dupe();
+                tokio::spawn(async move {
+                    let tcpstream = s_socket.accept().await.unwrap().0;
+                    let server =
+                        WebSocketStream::from_raw_socket(tcpstream, Role::Server, None).await;
+                    let mux = Multiplexor::new(server, OptionalDuration::NONE, false, None);
+                    let mut stream = mux.accept_stream_channel().await.unwrap();
+                    for _ in 0..num_writes {
+                        stream.write_all(&s_payload).await.unwrap();
+                    }
+                    stream.shutdown().await.unwrap();
+                });
+                let tcpstream = TcpStream::connect(s_addr).await.unwrap();
+                let client = WebSocketStream::from_raw_socket(tcpstream, Role::Client, None).await;
+                let mux = Multiplexor::new(client, OptionalDuration::NONE, false, None);
+                let mut stream = mux.new_stream_channel(&[], 0).await.unwrap();
+                stream.shutdown().await.unwrap();
+                //tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                let mut buf = vec![0; payload.len()];
+                for _ in 0..num_writes {
+                    stream.read_exact(&mut buf).await.unwrap();
+                    // No check for correctness as this should be guaranteed by tests
+                }
+            });
+        });
 }
 
 fn main() {
+    setup_logging();
     divan::main();
 }
