da97d935430322eaeccbc6686a7a748bb7045ed2
Zhang Maiyun <me@maiyun.me>
2025-05-03 07:27:49 -0700

    Implement multiple server datagrams

--- src/server/forwarder.rs
+++ src/server/forwarder.rs
@@ -4,13 +4,13 @@
 // SPDX-License-Identifier: Apache-2.0 OR GPL-3.0-or-later
 
 use crate::config;
-use penguin_mux::{Datagram, Dupe};
+use penguin_mux::{Datagram, Dupe, MuxStream};
 use std::net::SocketAddr;
 use thiserror::Error;
 use tokio::net::TcpStream;
 use tokio::{
     net::{UdpSocket, lookup_host},
-    sync::mpsc::Sender,
+    sync::mpsc,
 };
 use tracing::{debug, trace};
 
@@ -24,10 +24,10 @@ pub(super) enum Error {
 }
 
 /// Bind a UDP socket with the same address family as the given target,
-/// connect to the target, and send the given data.
-/// Finally, return the bound socket and the target address.
+/// and return the bound socket and the matched target address.
+/// Note that we don't connect or send the socket here.
 #[inline]
-async fn bind_and_send(target: (&str, u16), data: &[u8]) -> Result<(UdpSocket, SocketAddr), Error> {
+async fn bind_for_target(target: (&str, u16)) -> Result<(UdpSocket, SocketAddr), Error> {
     let targets = lookup_host(target).await?;
     let mut last_err = None;
     for target in targets {
@@ -47,11 +47,6 @@ async fn bind_and_send(target: (&str, u16), data: &[u8]) -> Result<(UdpSocket, S
             .local_addr()
             .expect("Failed to get local address of UDP socket (this is a bug)");
         debug!("bound to {local_addr}");
-        if let Err(e) = socket.connect(target).await {
-            last_err = Some(e);
-            continue;
-        }
-        socket.send(data).await?;
         return Ok((socket, target));
     }
     Err(last_err
@@ -64,43 +59,60 @@ async fn bind_and_send(target: (&str, u16), data: &[u8]) -> Result<(UdpSocket, S
         .into())
 }
 
-/// Send a UDP datagram to the given host and port and wait for a response
-/// in the following `UDP_PRUNE_TIMEOUT` seconds.
-#[tracing::instrument(skip(datagram_tx), level = "debug")]
-pub(super) async fn udp_forward_to(
-    datagram_frame: Datagram,
-    datagram_tx: Sender<Datagram>,
+/// Sit on a random port, send a UDP datagram to the given target,
+/// and wait for a response in the following `UDP_PRUNE_TIMEOUT` seconds.
+#[tracing::instrument(skip_all, level = "debug", fields(flow_id = first_datagram_frame.flow_id))]
+pub(super) async fn udp_forward_on(
+    first_datagram_frame: Datagram,
+    mut datagram_rx: mpsc::Receiver<Datagram>,
+    datagram_tx: mpsc::Sender<Datagram>,
 ) -> Result<(), Error> {
-    trace!("got datagram frame: {datagram_frame:?}");
-    let rhost = datagram_frame.target_host;
+    trace!("got datagram frame: {first_datagram_frame:?}");
+    let Datagram {
+        target_host: rhost,
+        target_port: rport,
+        flow_id,
+        data,
+    } = first_datagram_frame;
     let rhost_str = std::str::from_utf8(&rhost)?;
-    let rport = datagram_frame.target_port;
-    let data = datagram_frame.data;
-    let client_id = datagram_frame.flow_id;
-    let (socket, target) = bind_and_send((rhost_str, rport), data.as_ref()).await?;
+    let (socket, target) = bind_for_target((rhost_str, rport)).await?;
+    socket.send_to(&data, target).await?;
     trace!("sent UDP packet to {target}");
     loop {
+        // Reset this timeout each time we see traffic
+        let this_round_timeout = tokio::time::sleep(config::UDP_PRUNE_TIMEOUT);
         let mut buf = vec![0; config::MAX_UDP_PACKET_SIZE];
-        match tokio::time::timeout(config::UDP_PRUNE_TIMEOUT, socket.recv(&mut buf)).await {
-            Ok(Ok(len)) => {
-                trace!("got UDP response from {target}");
+        tokio::select! {
+            // Check if the socket has received a datagram
+            Ok((len, addr)) = socket.recv_from(&mut buf) => {
                 buf.truncate(len);
-                let datagram_frame = Datagram {
-                    flow_id: client_id,
+                trace!("got UDP response from {addr}");
+                let frame = Datagram {
                     target_host: rhost.dupe(),
                     target_port: rport,
+                    flow_id,
                     data: buf.into(),
                 };
-                if datagram_tx.send(datagram_frame).await.is_err() {
-                    // The main loop has exited, so we should exit too.
-                    break;
+                if datagram_tx.send(frame).await.is_err() {
+                    // Mux loop has exited
+                    return Ok(());
                 }
             }
-            Ok(Err(e)) => {
-                return Err(e.into());
+            // Check if the channel has received a datagram
+            Some(datagram_frame) = datagram_rx.recv() => {
+                // If this returns `None`, the mux loop has exited
+                // I don't want to handle this case here because
+                // the timeout branch will handle it for us anyway.
+                let target = (
+                    std::str::from_utf8(&datagram_frame.target_host)?,
+                    datagram_frame.target_port,
+                );
+                trace!("got new datagram frame: {datagram_frame:?} for {target:?}");
+                socket.send_to(&datagram_frame.data, target).await?;
             }
-            Err(_) => {
-                trace!("UDP prune timeout");
+            // Check if the timeout has expired
+            _ = this_round_timeout => {
+                trace!("UDP prune timeout expired");
                 break;
             }
         }
@@ -116,10 +128,8 @@ pub(super) async fn udp_forward_to(
 ///
 /// # Errors
 /// It carries the errors from the underlying TCP or channel IO functions.
-#[tracing::instrument(skip(channel), level = "debug")]
-pub(super) async fn tcp_forwarder_on_channel(
-    channel: super::websocket::MuxStream,
-) -> Result<(), Error> {
+#[tracing::instrument(skip_all, level = "debug")]
+pub(super) async fn tcp_forwarder_on_channel(channel: MuxStream) -> Result<(), Error> {
     let rhost = std::str::from_utf8(&channel.dest_host)?;
     let rport = channel.dest_port;
     trace!("attempting TCP connect to {rhost} port={rport}");
@@ -141,14 +151,15 @@ mod tests {
         crate::tests::setup_logging();
         let target_sock = UdpSocket::bind(("127.0.0.1", 0)).await.unwrap();
         let target_addr = target_sock.local_addr().unwrap();
-        let (socket, target) = bind_and_send(("127.0.0.1", target_addr.port()), b"hello")
+        let (socket, target) = bind_for_target(("127.0.0.1", target_addr.port()))
             .await
             .unwrap();
         assert_eq!(target, target_addr);
+        socket.send_to(b"hello", target).await.unwrap();
         let mut buf = vec![0; 5];
         let (len, addr) = target_sock.recv_from(&mut buf).await.unwrap();
         assert_eq!(len, 5);
-        assert_eq!(addr, socket.local_addr().unwrap());
+        assert_eq!(addr.port(), socket.local_addr().unwrap().port());
         assert_eq!(buf, b"hello");
         target_sock.send_to(b"world", addr).await.unwrap();
         socket.recv(&mut buf).await.unwrap();
@@ -160,14 +171,13 @@ mod tests {
         crate::tests::setup_logging();
         let target_sock = UdpSocket::bind(("::1", 0)).await.unwrap();
         let target_addr = target_sock.local_addr().unwrap();
-        let (socket, target) = bind_and_send(("::1", target_addr.port()), b"hello")
-            .await
-            .unwrap();
+        let (socket, target) = bind_for_target(("::1", target_addr.port())).await.unwrap();
         assert_eq!(target, target_addr);
+        socket.send_to(b"hello", target).await.unwrap();
         let mut buf = vec![0; 5];
         let (len, addr) = target_sock.recv_from(&mut buf).await.unwrap();
         assert_eq!(len, 5);
-        assert_eq!(addr, socket.local_addr().unwrap());
+        assert_eq!(addr.port(), socket.local_addr().unwrap().port());
         assert_eq!(buf, b"hello");
         target_sock.send_to(b"world", addr).await.unwrap();
         socket.recv(&mut buf).await.unwrap();
@@ -179,14 +189,16 @@ mod tests {
         crate::tests::setup_logging();
         let target_sock = UdpSocket::bind(("127.0.0.1", 0)).await.unwrap();
         let target_addr = target_sock.local_addr().unwrap();
-        let (tx, mut rx) = tokio::sync::mpsc::channel(4);
+        let (recv_tx, mut recv_rx) = tokio::sync::mpsc::channel(4);
+        let (send_tx, send_rx) = tokio::sync::mpsc::channel(4);
         let datagram_frame = Datagram {
             flow_id: 0,
             target_host: Bytes::from_static(b"127.0.0.1"),
             target_port: target_addr.port(),
             data: Bytes::from_static(b"hello"),
         };
-        let forwarder = tokio::spawn(udp_forward_to(datagram_frame, tx));
+        drop(send_tx);
+        let forwarder = tokio::spawn(udp_forward_on(datagram_frame, send_rx, recv_tx));
         let mut buf = vec![0; 5];
         let (len, addr) = target_sock.recv_from(&mut buf).await.unwrap();
         assert_eq!(len, 5);
@@ -195,11 +207,11 @@ mod tests {
         target_sock.send_to(b"test 2", addr).await.unwrap();
         target_sock.send_to(b"test 3", addr).await.unwrap();
         forwarder.await.unwrap().unwrap();
-        let datagram_frame = rx.recv().await.unwrap();
+        let datagram_frame: Datagram = recv_rx.recv().await.unwrap();
         assert_eq!(*datagram_frame.data, *b"test 1");
-        let datagram_frame = rx.recv().await.unwrap();
+        let datagram_frame = recv_rx.recv().await.unwrap();
         assert_eq!(*datagram_frame.data, *b"test 2");
-        let datagram_frame = rx.recv().await.unwrap();
+        let datagram_frame = recv_rx.recv().await.unwrap();
         assert_eq!(*datagram_frame.data, *b"test 3");
     }
 
@@ -208,14 +220,16 @@ mod tests {
         crate::tests::setup_logging();
         let target_sock = UdpSocket::bind(("::1", 0)).await.unwrap();
         let target_addr = target_sock.local_addr().unwrap();
-        let (tx, mut rx) = tokio::sync::mpsc::channel(4);
+        let (recv_tx, mut recv_rx) = tokio::sync::mpsc::channel(4);
+        let (send_tx, send_rx) = tokio::sync::mpsc::channel(4);
         let datagram_frame = Datagram {
             flow_id: 0,
             target_host: Bytes::from_static(b"::1"),
             target_port: target_addr.port(),
             data: Bytes::from_static(b"hello"),
         };
-        let forwarder = tokio::spawn(udp_forward_to(datagram_frame, tx));
+        drop(send_tx);
+        let forwarder = tokio::spawn(udp_forward_on(datagram_frame, send_rx, recv_tx));
         let mut buf = vec![0; 5];
         let (len, addr) = target_sock.recv_from(&mut buf).await.unwrap();
         assert_eq!(len, 5);
@@ -224,11 +238,11 @@ mod tests {
         target_sock.send_to(b"test 2", addr).await.unwrap();
         target_sock.send_to(b"test 3", addr).await.unwrap();
         forwarder.await.unwrap().unwrap();
-        let datagram_frame = rx.recv().await.unwrap();
+        let datagram_frame = recv_rx.recv().await.unwrap();
         assert_eq!(*datagram_frame.data, *b"test 1");
-        let datagram_frame = rx.recv().await.unwrap();
+        let datagram_frame = recv_rx.recv().await.unwrap();
         assert_eq!(*datagram_frame.data, *b"test 2");
-        let datagram_frame = rx.recv().await.unwrap();
+        let datagram_frame = recv_rx.recv().await.unwrap();
         assert_eq!(*datagram_frame.data, *b"test 3");
     }
 }
--- src/server/websocket.rs
+++ src/server/websocket.rs
@@ -4,13 +4,12 @@
 
 use super::WebSocket;
 use super::forwarder::tcp_forwarder_on_channel;
-use super::forwarder::udp_forward_to;
+use super::forwarder::udp_forward_on;
 use crate::config;
 use penguin_mux::{Datagram, Dupe, Multiplexor};
+use std::collections::HashMap;
 use tokio::{sync::mpsc, task::JoinSet};
 use tracing::{debug, error, trace, warn};
-
-pub use penguin_mux::MuxStream;
 
 /// Multiplex the `WebSocket` connection and handle the forwarding requests.
 #[tracing::instrument(skip(ws_stream), level = "debug")]
@@ -21,6 +20,7 @@ pub async fn handle_websocket(ws_stream: WebSocket, reverse: bool) {
         0
     });
     let mux = Multiplexor::new(ws_stream, Some(options), None);
+    let mut udp_clients: HashMap<u32, mpsc::Sender<Datagram>> = HashMap::new();
     debug!("WebSocket connection established");
     let mut jobs = JoinSet::new();
     // Channel for listeners to send UDP datagrams to the main loop
@@ -47,7 +47,20 @@ pub async fn handle_websocket(ws_stream: WebSocket, reverse: bool) {
             }
             // Check if the multiplexor has received a UDP datagram
             Ok(datagram_frame) = mux.get_datagram() => {
-                jobs.spawn(udp_forward_to(datagram_frame, datagram_send_tx.dupe()));
+                let flow_id = datagram_frame.flow_id;
+                if let Some(sender) = udp_clients.get_mut(&flow_id) {
+                    sender.send(datagram_frame).await.unwrap_or_else(|_| {
+                        // This client has been pruned, so we should
+                        // remove it from the map and hopefully
+                        // the client will try again.
+                        trace!("UDP client {flow_id} has been pruned");
+                        udp_clients.remove(&flow_id);
+                });
+                } else {
+                    let (sender, receiver) = mpsc::channel::<Datagram>(config::INCOMING_DATAGRAM_BUFFER_SIZE);
+                    udp_clients.insert(flow_id, sender);
+                    jobs.spawn(udp_forward_on(datagram_frame, receiver, datagram_send_tx.dupe()));
+                }
             }
             // Check if any of the listeners have sent a UDP datagram
             Some(datagram_frame) = datagram_send_rx.recv() => {
