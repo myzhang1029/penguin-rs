26198807f54ce486b54802d12da9cc1025674f28
Zhang Maiyun <me@maiyun.me>
2025-06-04 09:33:27 -0700

    Add tests for io_with_timeout

--- src/server/io_with_timeout.rs
+++ src/server/io_with_timeout.rs
@@ -5,6 +5,7 @@ use std::{
 };
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 
+/// A wrapper around an `AsyncRead` with a read timeout.
 pub struct IoWithTimeout<S> {
     stream: S,
     timeout: Option<Duration>,
@@ -60,9 +61,6 @@ impl<S: AsyncWrite + Send + Unpin> AsyncWrite for IoWithTimeout<S> {
         cx: &mut std::task::Context<'_>,
         buf: &[u8],
     ) -> Poll<std::io::Result<usize>> {
-        if self.elapsed() {
-            return Poll::Ready(Err(std::io::ErrorKind::TimedOut.into()));
-        }
         let this = self.get_mut();
         let stream = Pin::new(&mut this.stream);
         let result = ready!(stream.poll_write(cx, buf));
@@ -75,9 +73,6 @@ impl<S: AsyncWrite + Send + Unpin> AsyncWrite for IoWithTimeout<S> {
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<std::io::Result<()>> {
-        if self.elapsed() {
-            return Poll::Ready(Err(std::io::ErrorKind::TimedOut.into()));
-        }
         let this = self.get_mut();
         let stream = Pin::new(&mut this.stream);
         let result = ready!(stream.poll_flush(cx));
@@ -90,14 +85,76 @@ impl<S: AsyncWrite + Send + Unpin> AsyncWrite for IoWithTimeout<S> {
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<std::io::Result<()>> {
-        if self.elapsed() {
-            return Poll::Ready(Err(std::io::ErrorKind::TimedOut.into()));
-        }
         let this = self.get_mut();
         let stream = Pin::new(&mut this.stream);
         let result = ready!(stream.poll_shutdown(cx));
         // If the shutdown operation is `Ready`, reset the deadline
         this.reset();
         Poll::Ready(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+    #[tokio::test]
+    async fn test_read_will_timeout() {
+        let (reader, mut writer) = tokio::io::simplex(1024);
+        let mut io = IoWithTimeout::new(reader, Some(Duration::from_millis(100)));
+
+        tokio::spawn(async move {
+            // Delay the write more than the timeout
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            let _ = writer.write_all(b"hello").await;
+        });
+
+        let mut buf = vec![0; 5];
+        let result = io.read_exact(&mut buf).await;
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_read_can_succeed() {
+        let (reader, mut writer) = tokio::io::simplex(1024);
+        let mut io = IoWithTimeout::new(reader, Some(Duration::from_secs(1)));
+
+        tokio::spawn(async move {
+            // Write before the timeout
+            let _ = writer.write_all(b"hello").await;
+        });
+
+        let mut buf = vec![0; 5];
+        let result = io.read_exact(&mut buf).await;
+        assert!(result.is_ok());
+        assert_eq!(&buf, b"hello");
+    }
+
+    #[tokio::test]
+    async fn test_write_also_reset_deadline() {
+        let (us, mut task) = tokio::io::duplex(1024);
+        let mut io = IoWithTimeout::new(us, Some(Duration::from_secs(1)));
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 5];
+            // Read from our end
+            let _ = task.read_exact(&mut buf).await;
+            // Delay a bit more
+            tokio::time::sleep(Duration::from_millis(600)).await;
+            // Write to the writer
+            let _ = task.write_all(b"hello").await;
+        });
+
+        let mut buf = vec![0; 5];
+        // Delay a little
+        tokio::time::sleep(Duration::from_millis(500)).await;
+        // Write to reset the deadline
+        let result = io.write_all(b"hello").await;
+        assert!(result.is_ok());
+        // Now read and check if it succeeds
+        let result = io.read_exact(&mut buf).await;
+        assert!(result.is_ok());
+        assert_eq!(&buf, b"hello");
     }
 }
