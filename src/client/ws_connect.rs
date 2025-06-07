2185c26567c224c6f3f52c2e1f319031009f342e
Zhang Maiyun <me@maiyun.me>
2025-06-06 19:57:20 -0700

    Reorganize helper functions

--- src/client/ws_connect.rs
+++ src/client/ws_connect.rs
@@ -9,11 +9,11 @@ use penguin_mux::{Dupe, PROTOCOL_VERSION};
 use tokio::net::TcpStream;
 use tokio_tungstenite::tungstenite::{client::IntoClientRequest, handshake::client::Request};
 use tokio_tungstenite::{WebSocketStream, client_async};
-use tracing::{debug, trace, warn};
+use tracing::{debug, warn};
 
 /// Perform a `WebSocket` handshake.
 #[tracing::instrument(skip_all, fields(server = %args.server.0), level = "debug")]
-pub async fn handshake(
+async fn handshake_inner(
     args: &ClientArgs,
 ) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, super::Error> {
     // We already sanitized https URLs to wss
@@ -65,40 +65,16 @@ pub async fn handshake(
     for header in &args.header {
         req_headers.insert(&header.name, header.value.dupe());
     }
-    tokio::select! {
-        result = connect_handshake(
-            host, port, domain, is_tls,
-            args.tls_cert.as_deref(),
-            args.tls_key.as_deref(),
-            args.tls_ca.as_deref(),
-            args.tls_skip_verify,
-            req
-        ) => result,
-        () = args.handshake_timeout.sleep() => Err(super::Error::HandshakeTimeout),
-        Ok(()) = tokio::signal::ctrl_c() => Err(super::Error::HandshakeCancelled),
-    }
-}
-async fn connect_handshake(
-    host: &str,
-    port: u16,
-    domain: &str,
-    is_tls: bool,
-    tls_cert: Option<&str>,
-    tls_key: Option<&str>,
-    tls_ca: Option<&str>,
-    tls_skip_verify: bool,
-    req: Request,
-) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, super::Error> {
     let stream = if is_tls {
         MaybeTlsStream::Tls(
             tls_connect(
                 host,
                 port,
                 domain,
-                tls_cert,
-                tls_key,
-                tls_ca,
-                tls_skip_verify,
+                args.tls_cert.as_deref(),
+                args.tls_key.as_deref(),
+                args.tls_ca.as_deref(),
+                args.tls_skip_verify,
             )
             .await?,
         )
@@ -106,7 +82,7 @@ async fn connect_handshake(
         // No TLS
         warn!("Using insecure WebSocket connection");
         MaybeTlsStream::Plain(
-            tcp_connect(host, port)
+            TcpStream::connect((host, port))
                 .await
                 .map_err(super::Error::TcpConnect)?,
         )
@@ -116,7 +92,12 @@ async fn connect_handshake(
     Ok(ws_stream)
 }
 
-async fn tcp_connect(host: &str, port: u16) -> std::io::Result<TcpStream> {
-    trace!("connecting to {host}:{port}");
-    TcpStream::connect((host, port)).await
+pub async fn handshake(
+    args: &ClientArgs,
+) -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, super::Error> {
+    tokio::select! {
+        result = handshake_inner(args) => result,
+        () = args.handshake_timeout.sleep() => Err(super::Error::HandshakeTimeout),
+        Ok(()) = tokio::signal::ctrl_c() => Err(super::Error::HandshakeCancelled),
+    }
 }
