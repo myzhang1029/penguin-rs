8706bd61000cafb295c961cb1802e18d3db04a0f
Zhang Maiyun <me@maiyun.me>
2026-06-10 19:11:19 -0400

    Fuzzer for remote parser

---
+++ fuzz/fuzz_targets/remote_parser.rs
@@ -0,0 +1,8 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    let string = String::from_utf8_lossy(data);
+    let _: Option<rusty_penguin_lib::parse_remote::Remote> = string.parse().ok();
+});
