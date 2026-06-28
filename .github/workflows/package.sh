648257c8f7fc43720dff181aedf7d46ef7caad01
Zhang Maiyun <me@maiyun.me>
2026-06-28 15:32:56 -0400

    Delete old nightly releases in the action script

--- .github/workflows/package.sh
+++ .github/workflows/package.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+set -euo pipefail
+
 TARGET="$1"
 PROFILE="$2"
+REPO="myzhang1029/penguin-rs"
 GIT_REV="$(git describe --tags --match='v[0-9\.]*' --long | cut -f2 -d-)"
 if [ "$GIT_REV" -eq 0 ]; then
   PKG_REV="$((GIT_REV + 1)).gha"
@@ -19,3 +23,15 @@ cargo generate-rpm --package penguin \
   -s "release = \"$PKG_REV\"" \
   --target "$TARGET" --profile "$PROFILE"
 cp -v target/"$TARGET"/generate-rpm/*.rpm packages
+
+# Delete old packages
+if [ "$GIT_REV" -ne 0 ]; then
+  TAIL_PATTERN="$(ls packages | sed 's/.*gha\(.*\)/\1/' | sort | uniq | tr '\n' '|' | sed 's/\(.*\)|/(\1)/')"
+  ASSET_IDS="$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2026-03-10" \
+    "/repos/$REPO/releases" | jq "map(select(.tag_name==\"nightly\"))[0] | .assets | map(select(.name | test(\"^rusty-penguin.*$TAIL_PATTERN\$\")) | .id) | .[]")"
+  for asset_id in $ASSET_IDS; do
+    echo "DELETE /repos/$REPO/releases/assets/$asset_id"
+    gh api --method DELETE -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2026-03-10" \
+      "/repos/$REPO/releases/assets/$asset_id"
+  done
+fi
