cae77f348a5d986684c7a51239c5ae3eab1edc3f
Zhang Maiyun <me@maiyun.me>
2026-06-30 08:19:22 -0400

    Only delete old packages when pushing to main or tag

--- .github/workflows/package.sh
+++ .github/workflows/package.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 TARGET="$1"
 PROFILE="$2"
+DELETE_OLD="$3"
 REPO="myzhang1029/penguin-rs"
 GIT_REV="$(git describe --tags --match='v[0-9\.]*' --long | cut -f2 -d-)"
 if [ "$GIT_REV" -eq 0 ]; then
@@ -25,7 +26,7 @@ cargo generate-rpm --package penguin \
 cp -v target/"$TARGET"/generate-rpm/*.rpm packages
 
 # Delete old packages
-if [ "$GIT_REV" -ne 0 ]; then
+if [ "$GIT_REV" -ne 0 ] && [ "$DELETE_OLD" = true ]; then
   TAIL_PATTERN="$(ls packages | sed 's/.*gha\(.*\)/\1/' | sort | uniq | tr '\n' '|' | sed 's/\(.*\)|/(\1)/')"
   ASSET_IDS="$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2026-03-10" \
     "/repos/$REPO/releases" | jq "map(select(.tag_name==\"nightly\"))[0] | .assets | map(select(.name | test(\"^rusty-penguin.*$TAIL_PATTERN\$\")) | .id) | .[]")"
