f2e4f11a1b6c4656bbee8dadba37b411bbcadd78
Zhang Maiyun <me@maiyun.me>
2026-07-05 22:08:15 -0400

    Make bench reproducible

--- penguin-mux/benches/stream_throughput.rs
+++ penguin-mux/benches/stream_throughput.rs
@@ -1,6 +1,7 @@
 use bytes::Bytes;
 use divan::{Bencher, counter::BytesCount};
-use penguin_mux::Multiplexor;
+use penguin_mux::{Multiplexor, config::Options};
+use rand::{Rng, SeedableRng};
 use std::sync::LazyLock;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -28,7 +29,10 @@ static TOKIO_RT: LazyLock<runtime::Runtime> = LazyLock::new(|| {
 const EACH_WRITE_SIZE: usize = 4096;
 
 fn make_payload() -> Bytes {
-    (0..EACH_WRITE_SIZE).map(|_| rand::random::<u8>()).collect()
+    let mut rng = rand::rngs::SmallRng::seed_from_u64(0xabcd);
+    let mut payload = vec![0u8; EACH_WRITE_SIZE];
+    rng.fill_bytes(&mut payload);
+    payload.into()
 }
 
 #[divan::bench]
@@ -37,16 +41,25 @@ fn baseline_ws(b: Bencher<'_, '_>) {
         TOKIO_RT.block_on(async {
             let s_socket = TcpListener::bind(("::1", 0)).await.unwrap();
             let s_addr = s_socket.local_addr().unwrap();
+            let mut rng = rand::rngs::SmallRng::seed_from_u64(0xabcd);
+            let rng2 = rng.fork();
             tokio::spawn(async move {
                 let tcpstream = s_socket.accept().await.unwrap().0;
                 let server = WebSocketStream::from_raw_socket(tcpstream, Role::Server, None).await;
-                let mux = Multiplexor::new(server);
+                let (mux, task) = Multiplexor::new_detailed::<_, std::time::Instant>(
+                    server,
+                    Options::default(),
+                    rng2,
+                );
+                task.spawn(None);
                 let mut stream = mux.accept_stream_channel().await.unwrap();
                 stream.shutdown().await.unwrap();
             });
             let tcpstream = TcpStream::connect(s_addr).await.unwrap();
             let client = WebSocketStream::from_raw_socket(tcpstream, Role::Client, None).await;
-            let mux = Multiplexor::new(client);
+            let (mux, task) =
+                Multiplexor::new_detailed::<_, std::time::Instant>(client, Options::default(), rng);
+            task.spawn(None);
             let mut stream = mux.new_stream_channel(&[], 0).await.unwrap();
             stream.shutdown().await.unwrap();
         });
@@ -124,11 +137,18 @@ fn bench_stream_throughput(b: Bencher<'_, '_>, num_writes: usize) {
                 let s_addr = s_socket.local_addr().unwrap();
                 let s_payload = payload.clone(); // cheap
                 let len = payload.len();
+                let mut rng = rand::rngs::SmallRng::seed_from_u64(0xabcd);
+                let rng2 = rng.fork();
                 tokio::spawn(async move {
                     let tcpstream = s_socket.accept().await.unwrap().0;
                     let server =
                         WebSocketStream::from_raw_socket(tcpstream, Role::Server, None).await;
-                    let mux = Multiplexor::new(server);
+                    let (mux, task) = Multiplexor::new_detailed::<_, std::time::Instant>(
+                        server,
+                        Options::default(),
+                        rng2,
+                    );
+                    task.spawn(None);
                     let mut stream = mux.accept_stream_channel().await.unwrap();
                     for _ in 0..num_writes {
                         stream.write_all(&s_payload).await.unwrap();
@@ -137,7 +157,12 @@ fn bench_stream_throughput(b: Bencher<'_, '_>, num_writes: usize) {
                 });
                 let tcpstream = TcpStream::connect(s_addr).await.unwrap();
                 let client = WebSocketStream::from_raw_socket(tcpstream, Role::Client, None).await;
-                let mux = Multiplexor::new(client);
+                let (mux, task) = Multiplexor::new_detailed::<_, std::time::Instant>(
+                    client,
+                    Options::default(),
+                    rng,
+                );
+                task.spawn(None);
                 let mut stream = mux.new_stream_channel(&[], 0).await.unwrap();
                 stream.shutdown().await.unwrap();
                 //tokio::time::sleep(core::time::Duration::from_secs(1)).await;
@@ -160,11 +185,18 @@ fn bench_stream_throughput_bidir(b: Bencher<'_, '_>, num_writes: usize) {
                 let s_addr = s_socket.local_addr().unwrap();
                 let s_payload = payload.clone(); // cheap
                 let len = payload.len();
+                let mut rng = rand::rngs::SmallRng::seed_from_u64(0xabcd);
+                let rng2 = rng.fork();
                 tokio::spawn(async move {
                     let tcpstream = s_socket.accept().await.unwrap().0;
                     let server =
                         WebSocketStream::from_raw_socket(tcpstream, Role::Server, None).await;
-                    let mux = Multiplexor::new(server);
+                    let (mux, task) = Multiplexor::new_detailed::<_, std::time::Instant>(
+                        server,
+                        Options::default(),
+                        rng2,
+                    );
+                    task.spawn(None);
                     let mut stream = mux.accept_stream_channel().await.unwrap();
                     let mut buf = vec![0; len];
                     for _ in 0..num_writes {
@@ -176,7 +208,12 @@ fn bench_stream_throughput_bidir(b: Bencher<'_, '_>, num_writes: usize) {
                 });
                 let tcpstream = TcpStream::connect(s_addr).await.unwrap();
                 let client = WebSocketStream::from_raw_socket(tcpstream, Role::Client, None).await;
-                let mux = Multiplexor::new(client);
+                let (mux, task) = Multiplexor::new_detailed::<_, std::time::Instant>(
+                    client,
+                    Options::default(),
+                    rng,
+                );
+                task.spawn(None);
                 let mut stream = mux.new_stream_channel(&[], 0).await.unwrap();
                 //tokio::time::sleep(core::time::Duration::from_secs(1)).await;
                 let mut buf = vec![0; len];
@@ -203,12 +240,19 @@ fn bench_stream_throughput_with_contention(b: Bencher<'_, '_>, num_concurrent: u
                 let s_addr = s_socket.local_addr().unwrap();
                 let s_payload = payload.clone(); // cheap
                 let len = payload.len();
+                let mut rng = rand::rngs::SmallRng::seed_from_u64(0xabcd);
+                let rng2 = rng.fork();
                 jobs.spawn(async move {
                     let mut server_jobs = tokio::task::JoinSet::new();
                     let tcpstream = s_socket.accept().await.unwrap().0;
                     let server =
                         WebSocketStream::from_raw_socket(tcpstream, Role::Server, None).await;
-                    let mux = Multiplexor::new(server);
+                    let (mux, task) = Multiplexor::new_detailed::<_, std::time::Instant>(
+                        server,
+                        Options::default(),
+                        rng2,
+                    );
+                    task.spawn(None);
                     for _ in 0..num_concurrent {
                         let mut stream = mux.accept_stream_channel().await.unwrap();
                         let s_payload = s_payload.clone(); // cheap
@@ -228,7 +272,12 @@ fn bench_stream_throughput_with_contention(b: Bencher<'_, '_>, num_concurrent: u
                 });
                 let tcpstream = TcpStream::connect(s_addr).await.unwrap();
                 let client = WebSocketStream::from_raw_socket(tcpstream, Role::Client, None).await;
-                let mux = Multiplexor::new(client);
+                let (mux, task) = Multiplexor::new_detailed::<_, std::time::Instant>(
+                    client,
+                    Options::default(),
+                    rng,
+                );
+                task.spawn(None);
                 for _ in 0..num_concurrent {
                     let mut stream = mux.new_stream_channel(&[], 0).await.unwrap();
                     jobs.spawn(async move {
