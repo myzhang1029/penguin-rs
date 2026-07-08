6f1f728aee179e9191d1d92975736959d3d104df
Zhang Maiyun <me@maiyun.me>
2026-07-08 17:10:43 -0400

    Make test --verbose optional

--- .github/workflows/test-coverage.sh
+++ .github/workflows/test-coverage.sh
@@ -2,56 +2,57 @@
 
 TLS=$1
 CONFIG_NAME=$2
+VERBOSE=${3:---verbose}
 export RUSTFLAGS="-Cinstrument-coverage"
 
 # Run async-acceptor tests only
-cargo nextest run --all-targets --verbose --package async-acceptor
+cargo nextest run --all-targets "$VERBOSE" --package async-acceptor
 
 # Run cow-bytes tests only
-cargo nextest run --all-targets --verbose --package cow-bytes
+cargo nextest run --all-targets "$VERBOSE" --package cow-bytes
 # Run no_std cow-bytes tests only
-cargo nextest run --all-targets --verbose --package cow-bytes --no-default-features
+cargo nextest run --all-targets "$VERBOSE" --package cow-bytes --no-default-features
 
 # Run penguin-socks tests only
-cargo nextest run --all-targets --verbose --package penguin-socks
+cargo nextest run --all-targets "$VERBOSE" --package penguin-socks
 
 # Run penguin-mux tests with minimal features
-cargo nextest run --all-targets --verbose --no-default-features --package penguin-mux
+cargo nextest run --all-targets "$VERBOSE" --no-default-features --package penguin-mux
 
 # Note that tokio/rt is always on in penguin-mux tests
 
 # Run penguin-mux tests with tokio-io-util support
-cargo nextest run --all-targets --verbose --no-default-features --package penguin-mux --features tokio-io-util
+cargo nextest run --all-targets "$VERBOSE" --no-default-features --package penguin-mux --features tokio-io-util
 
 # Run cargo penguin-mux tests with tokio-time support
-cargo nextest run --all-targets --verbose --no-default-features --package penguin-mux --features tokio-time
+cargo nextest run --all-targets "$VERBOSE" --no-default-features --package penguin-mux --features tokio-time
 
 # Run cargo penguin-mux tests with tungstenite (essentially all features)
-cargo nextest run --all-targets --verbose --no-default-features --package penguin-mux --features std,tokio,tungstenite
+cargo nextest run --all-targets "$VERBOSE" --no-default-features --package penguin-mux --features std,tokio,tungstenite
 
 
 
 # Run cargo tests with more features on
-cargo nextest run --all-targets --verbose --features "$TLS",ring,tests-real-internet4,tests-acme-has-pebble,penguin-binary,acme,default-is-ipv6,tokio-console,deadlock-detection --no-default-features
+cargo nextest run --all-targets "$VERBOSE" --features "$TLS",ring,tests-real-internet4,tests-acme-has-pebble,penguin-binary,acme,default-is-ipv6,tokio-console,deadlock-detection --no-default-features
 
 # Run cargo tests with default features
 case "$CONFIG_NAME" in
   *linux-gnu)
     printf '[target.%s]\nrunner="sudo -E"' "$(rustc -vV | sed -n 's,host: ,,p')" > ~/.cargo/config.toml
-    cargo nextest run --all-targets --verbose --features "$TLS",ring,tests-real-internet4,acme,penguin-binary,tproxy --no-default-features
+    cargo nextest run --all-targets "$VERBOSE" --features "$TLS",ring,tests-real-internet4,acme,penguin-binary,tproxy --no-default-features
     rm ~/.cargo/config.toml
     ;;
   *)
-    cargo nextest run --all-targets --verbose --features "$TLS",ring,tests-real-internet4,acme,penguin-binary,tproxy --no-default-features
+    cargo nextest run --all-targets "$VERBOSE" --features "$TLS",ring,tests-real-internet4,acme,penguin-binary,tproxy --no-default-features
     ;;
 esac
 
 # Run cargo tests with the nohash hashmap
-cargo nextest run --all-targets --verbose --features "$TLS",ring,tests-real-internet4,acme,penguin-binary,nohash --no-default-features
+cargo nextest run --all-targets "$VERBOSE" --features "$TLS",ring,tests-real-internet4,acme,penguin-binary,nohash --no-default-features
 
 # Run cargo tests with PENGUIN_TLS_CHROMIUM_LIKE enabled
 if [ "$TLS" != "nativetls" ]; then
-  PENGUIN_TLS_CHROMIUM_LIKE=on cargo nextest run --all-targets --verbose --features "$TLS",ring,tests-real-internet4,acme,penguin-binary --no-default-features
+  PENGUIN_TLS_CHROMIUM_LIKE=on cargo nextest run --all-targets "$VERBOSE" --features "$TLS",ring,tests-real-internet4,acme,penguin-binary --no-default-features
 fi
 
 
