164dc2cd1c9c9c9a511cda964fef43b1c4c6873a
Zhang Maiyun <me@maiyun.me>
2026-07-17 21:03:15 -0400

    Make test_copy_bidirectional_normal run in loom

--- penguin-mux/src/stream_tools/copy_bidirectional.rs
+++ penguin-mux/src/stream_tools/copy_bidirectional.rs
@@ -189,15 +189,13 @@ where
 #[cfg(feature = "tokio-io-util")]
 mod tests {
     use crate::frame::{Frame, Payload, PushPayload};
+    use crate::loom::{Arc, AtomicBool, AtomicU32, AtomicWaker};
     use crate::stream::MuxStream;
     use crate::tests::setup_logging;
     use crate::ws::Message::Binary;
-    use alloc::sync::Arc;
     use bytes::Bytes;
     use core::pin::Pin;
-    use core::sync::atomic::{AtomicBool, AtomicU32};
     use core::task::{Context, Poll};
-    use futures_util::task::AtomicWaker;
     use tokio::io::{AsyncRead, ReadBuf};
     use tokio::io::{AsyncReadExt, AsyncWriteExt};
     use tokio::sync::mpsc;
@@ -207,12 +205,24 @@ mod tests {
     #[tokio::test]
     #[cfg(not(loom))]
     async fn test_copy_bidirectional_normal() {
+        setup_logging();
+        test_copy_bidirectional_normal_inner().await;
+    }
+
+    #[test]
+    #[cfg(loom)]
+    fn test_copy_bidirectional_normal_loom() {
+        loom::model(|| {
+            loom::future::block_on(test_copy_bidirectional_normal_inner());
+        })
+    }
+
+    async fn test_copy_bidirectional_normal_inner() {
         const TX1: Bytes = Bytes::from_static(b"hello from mux");
         const RX1: Bytes = Bytes::from_static(b"hello from other");
         const TX2: Bytes = Bytes::from_static(b"short");
         const RX2: Bytes = Bytes::from_static(b"hello after half-close");
         const RX3: Bytes = Bytes::from_static(b"stout");
-        setup_logging();
         let (rx_frame_tx, rx_frame_rx) = mpsc::channel(DEFAULT_RWND_THRESHOLD as usize);
         let (tx_msg_tx, mut tx_msg_rx) = mpsc::unbounded_channel();
         let (dropped_flows_tx, _) = mpsc::unbounded_channel();
@@ -233,81 +243,84 @@ mod tests {
             rwnd_threshold: DEFAULT_RWND_THRESHOLD,
         };
 
-        let copy_task = tokio::spawn(mux_stream.into_copy_bidirectional(other_stream));
+        let copy_task = mux_stream.into_copy_bidirectional(other_stream);
 
-        let waker = futures_util::task::noop_waker();
-        let mut cx = Context::from_waker(&waker);
-        let mut buf = [0u8; 14];
-        let mut rbuf = ReadBuf::new(&mut buf);
-        let rs = Pin::new(&mut check_side).poll_read(&mut cx, &mut rbuf);
-        assert!(matches!(rs, Poll::Pending));
+        let main_task = async {
+            let waker = futures_util::task::noop_waker();
+            let mut cx = Context::from_waker(&waker);
+            let mut buf = [0u8; 14];
+            let mut rbuf = ReadBuf::new(&mut buf);
+            let rs = Pin::new(&mut check_side).poll_read(&mut cx, &mut rbuf);
+            assert!(matches!(rs, Poll::Pending));
 
-        // Send data to the MuxStream
-        rx_frame_tx.send(TX1.clone()).await.unwrap();
-        let size = check_side.read(&mut buf).await.unwrap();
-        // Should be ready
-        assert_eq!(size, TX1.len());
-        assert_eq!(&buf[..size], TX1);
+            // Send data to the MuxStream
+            rx_frame_tx.send(TX1.clone()).await.unwrap();
+            let size = check_side.read(&mut buf).await.unwrap();
+            // Should be ready
+            assert_eq!(size, TX1.len());
+            assert_eq!(&buf[..size], TX1);
 
-        // Write to the AsyncWrite side
-        check_side.write_all(&RX1).await.unwrap();
-        // This side has buffering
-        check_side.flush().await.unwrap();
+            // Write to the AsyncWrite side
+            check_side.write_all(&RX1).await.unwrap();
+            // This side has buffering
+            check_side.flush().await.unwrap();
 
-        // Verify data was sent to the remote peer
-        let Binary(msg) = tx_msg_rx.recv().await.unwrap() else {
-            panic!("Expected a binary message");
+            // Verify data was sent to the remote peer
+            let Binary(msg) = tx_msg_rx.recv().await.unwrap() else {
+                panic!("Expected a binary message");
+            };
+            let frame = Frame::try_from(msg).unwrap();
+            assert_eq!(frame.id, 1);
+            if let Payload::Push(PushPayload::Single(push)) = frame.payload {
+                assert_eq!(push.as_ref(), RX1);
+            } else {
+                panic!("Expected a `Push(Single)` frame");
+            }
+
+            // Send some partial data before we go away to check that the
+            // data isn't lost in the process
+            rx_frame_tx.send(TX2.clone()).await.unwrap();
+            drop(rx_frame_tx);
+            // Check that the data is not lost
+            let read = check_side.read(&mut buf).await.unwrap();
+            assert_eq!(read, TX2.len());
+            assert_eq!(&buf[..read], TX2);
+            // Make sure this side is getting EOF
+            let m = check_side.read(&mut buf).await.unwrap();
+            assert_eq!(m, 0);
+            // Make sure that only this side is closed and not the other side
+            check_side.write_all(&RX2).await.unwrap();
+            check_side.flush().await.unwrap();
+            // Check that this side is still open
+            let Binary(msg) = tx_msg_rx.recv().await.unwrap() else {
+                panic!("Expected a binary message");
+            };
+            let frame = Frame::try_from(msg).unwrap();
+            assert_eq!(frame.id, 1);
+            if let Payload::Push(PushPayload::Single(push)) = frame.payload {
+                assert_eq!(push.as_ref(), RX2);
+            } else {
+                panic!("Expected a `Push(Single)` frame");
+            }
+            // Again short data before we go away
+            check_side.write_all(&RX3).await.unwrap();
+            check_side.shutdown().await.unwrap();
+            // Check that the data is not lost
+            let Binary(msg) = tx_msg_rx.recv().await.unwrap() else {
+                panic!("Expected a binary message");
+            };
+            let frame = Frame::try_from(msg).unwrap();
+            assert_eq!(frame.id, 1);
+            if let Payload::Push(PushPayload::Single(push)) = frame.payload {
+                assert_eq!(push.as_ref(), RX3);
+            } else {
+                panic!("Expected a `Push(Single)` frame");
+            }
         };
-        let frame = Frame::try_from(msg).unwrap();
-        assert_eq!(frame.id, 1);
-        if let Payload::Push(PushPayload::Single(push)) = frame.payload {
-            assert_eq!(push.as_ref(), RX1);
-        } else {
-            panic!("Expected a `Push(Single)` frame");
-        }
-
-        // Send some partial data before we go away to check that the
-        // data isn't lost in the process
-        rx_frame_tx.send(TX2.clone()).await.unwrap();
-        drop(rx_frame_tx);
-        // Check that the data is not lost
-        let read = check_side.read(&mut buf).await.unwrap();
-        assert_eq!(read, TX2.len());
-        assert_eq!(&buf[..read], TX2);
-        // Make sure this side is getting EOF
-        let m = check_side.read(&mut buf).await.unwrap();
-        assert_eq!(m, 0);
-        // Make sure that only this side is closed and not the other side
-        check_side.write_all(&RX2).await.unwrap();
-        check_side.flush().await.unwrap();
-        // Check that this side is still open
-        let Binary(msg) = tx_msg_rx.recv().await.unwrap() else {
-            panic!("Expected a binary message");
-        };
-        let frame = Frame::try_from(msg).unwrap();
-        assert_eq!(frame.id, 1);
-        if let Payload::Push(PushPayload::Single(push)) = frame.payload {
-            assert_eq!(push.as_ref(), RX2);
-        } else {
-            panic!("Expected a `Push(Single)` frame");
-        }
-        // Again short data before we go away
-        check_side.write_all(&RX3).await.unwrap();
-        check_side.shutdown().await.unwrap();
-        // Check that the data is not lost
-        let Binary(msg) = tx_msg_rx.recv().await.unwrap() else {
-            panic!("Expected a binary message");
-        };
-        let frame = Frame::try_from(msg).unwrap();
-        assert_eq!(frame.id, 1);
-        if let Payload::Push(PushPayload::Single(push)) = frame.payload {
-            assert_eq!(push.as_ref(), RX3);
-        } else {
-            panic!("Expected a `Push(Single)` frame");
-        }
 
         // Get final results
-        let (bytes_read, bytes_written) = copy_task.await.unwrap().unwrap();
+        let ((), r) = tokio::join!(main_task, copy_task);
+        let (bytes_read, bytes_written) = r.unwrap();
         // TX is copied from MuxStream to other_stream, which is `read_bytes` in `copy_bidirectional`
         assert_eq!(bytes_read, TX1.len() + TX2.len());
         assert_eq!(bytes_written, RX1.len() + RX2.len() + RX3.len());
