2051c5873f6e01161fa8a58e3e07a4b748e688d2
Zhang Maiyun <me@maiyun.me>
2026-08-04 10:00:42 -0400

    debug wip

--- penguin/src/tests/tcptunnelchecker.rs
+++ penguin/src/tests/tcptunnelchecker.rs
@@ -18,8 +18,6 @@ use crate::arg::{self, ClientArgs};
 use crate::arg::{Remote, default_host};
 use crate::client::{HandlerResources, client_main_inner};
 use crate::server::{State, run_listener};
-use std::cell::{Cell, RefCell, UnsafeCell};
-use std::fmt::Display;
 use std::io::{self, ErrorKind, Read, Write};
 use std::mem::MaybeUninit;
 use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream};
@@ -91,7 +89,7 @@ fn clog<W: Write>(mut s: W) -> io::Result<usize> {
     let buf = [0u8; 1024];
     let mut writelen = 1024;
     let mut waitctr = 6;
-    let mut written: usize = 0;
+    let mut written = 0;
     loop {
         match s.write(&buf[0..writelen]) {
             Ok(0) => break Err(ErrorKind::WriteZero.into()),
@@ -116,140 +114,30 @@ fn clog<W: Write>(mut s: W) -> io::Result<usize> {
     }
 }
 
-/// Read and ignore all the data in a separate thread
-fn drain<R: Read + Send + 'static>(mut s: R, close_notification: Arc<AtomicBool>) {
-    thread::spawn(move || {
-        let mut buf = [0u8; 1024];
-        loop {
-            match s.read(&mut buf) {
-                Ok(0) => break,
-                Ok(_x) => (),
-                Err(e) if e.kind() == ErrorKind::WouldBlock => {
-                    thread::sleep(Duration::from_millis(10))
-                }
-                Err(e) if e.kind() == ErrorKind::Interrupted => (),
-                Err(_e) => break,
-            }
-        }
-        close_notification.store(true, Ordering::SeqCst);
-    });
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum CloseDetectMode {
-    CloseIncomingCheckOutgoing,
-    CloseOutgoingCheckIncoming,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum WritingPolicy {
-    Ignore,
-    Shutdown,
-    Clog,
-}
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum ReadingPolicy {
-    Ignore,
-    Drain,
-}
-
-/// Clog both directions of the tunnel by writing, but not reading the data.
-/// Then close of of the sockets. Will RST propagate to the other end?
-fn test_inner(
-    mode: CloseDetectMode,
-    outgoing_write: WritingPolicy,
-    outgoing_read: ReadingPolicy,
-    incoming_write: WritingPolicy,
-    incoming_read: ReadingPolicy,
-) {
-    if (incoming_read, outgoing_write) == (ReadingPolicy::Drain, WritingPolicy::Clog) {
-        return;
-    }
-    if (outgoing_read, incoming_write) == (ReadingPolicy::Drain, WritingPolicy::Clog) {
-        return;
-    }
-    if mode == CloseDetectMode::CloseIncomingCheckOutgoing
-        && !matches!(
-            (outgoing_write, outgoing_read),
-            (WritingPolicy::Clog, _) | (_, ReadingPolicy::Drain)
-        )
-    {
-        return;
-    }
-    if mode == CloseDetectMode::CloseOutgoingCheckIncoming
-        && !matches!(
-            (incoming_write, incoming_read),
-            (WritingPolicy::Clog, _) | (_, ReadingPolicy::Drain)
-        )
-    {
-        return;
-    }
-
+#[test]
+fn test_inner() {
     setup_logging();
     let listener = TcpListener::bind(format!("{LOCALHOST}:0")).unwrap();
     let laddr = listener.local_addr().unwrap();
     let (taddr, s_worker, c_worker, should_stop_tx) = make_tunnel(laddr);
 
     let listen_thread = thread::spawn(move || {
-        let mut incoming_side: TcpStream = listener.accept().unwrap().0;
+        let incoming_side = listener.accept().unwrap().0;
         drop(listener);
         incoming_side.set_nonblocking(true).unwrap();
-        let cn = match incoming_read {
-            ReadingPolicy::Ignore => None,
-            ReadingPolicy::Drain => {
-                let cn = Arc::new(AtomicBool::new(true));
-                drain(incoming_side.try_clone().unwrap(), cn.clone());
-                Some(cn)
-            }
-        };
-        match incoming_write {
-            WritingPolicy::Ignore => (),
-            WritingPolicy::Shutdown => {
-                incoming_side.shutdown(Shutdown::Write).unwrap();
-                info!("`incoming_side` socket shutdown_wr");
-            }
-            WritingPolicy::Clog => {
-                let sz = clog(&mut incoming_side).unwrap();
-                info!("clogged `incoming_side` socket, size: {sz}");
-            }
-        }
-        // thread::sleep(Duration::from_millis(500));
-        (incoming_side, cn)
+        incoming_side.shutdown(Shutdown::Write).unwrap();
+        info!("`incoming_side` socket shutdown_wr");
+        incoming_side
     });
     let mut outgoing_side = TcpStream::connect(taddr).unwrap();
     outgoing_side.set_nonblocking(true).unwrap();
-    let cs_close = match outgoing_read {
-        ReadingPolicy::Ignore => None,
-        ReadingPolicy::Drain => {
-            let cs_close = Arc::new(AtomicBool::new(true));
-            drain(outgoing_side.try_clone().unwrap(), cs_close.clone());
-            Some(cs_close)
-        }
-    };
-    match outgoing_write {
-        WritingPolicy::Ignore => (),
-        WritingPolicy::Shutdown => {
-            outgoing_side.shutdown(Shutdown::Write).unwrap();
-            info!("`outgoing_side` socket shutdown_wr");
-        }
-        WritingPolicy::Clog => {
-            let sz = clog(&mut outgoing_side).unwrap();
-            info!("clogged `outgoing_side` socket, size: {sz}");
-        }
-    }
-    let (incoming_side, cc_close) = listen_thread.join().unwrap();
+    let sz = clog(&mut outgoing_side).unwrap();
+    info!("clogged `outgoing_side` socket, size: {sz}");
+    let incoming_side = listen_thread.join().unwrap();
     // Now both `cs` and `cc` sockets are fully clogged. Let's close one of them and see what happens to the other one.
     thread::sleep(Duration::from_millis(100));
-    let (s, closenotif) = match mode {
-        CloseDetectMode::CloseIncomingCheckOutgoing => {
-            drop(incoming_side);
-            (outgoing_side, cs_close)
-        }
-        CloseDetectMode::CloseOutgoingCheckIncoming => {
-            drop(outgoing_side);
-            (incoming_side, cc_close)
-        }
-    };
+    drop(incoming_side);
+    let s = outgoing_side;
 
     // Check if the other end can detect the closure of the socket
     let deadline = Instant::now() + Duration::from_secs(5);
@@ -258,11 +146,6 @@ fn test_inner(
             Instant::now() <= deadline,
             "The other end did not detect socket closure in time"
         );
-        if let Some(acn) = closenotif.as_ref()
-            && acn.load(Ordering::SeqCst)
-        {
-            break;
-        }
         let mut buf = [0u8; 256];
         let r = s.peek(&mut buf).and_then(|_| s.take_error());
         if let Ok(Some(e)) | Err(e) = r {
@@ -288,45 +171,3 @@ fn test_inner(
     s_worker.join().unwrap();
     c_worker.join().unwrap();
 }
-
-macro_rules! expand_tests {
-    ($m:expr, $ow:expr, $or:expr, $iw:expr, $ir:expr, $oid:ident, $iid:ident) => {
-        paste::paste! {
-            #[test]
-            #[expect(non_snake_case)]
-            #[ignore]
-            fn [<out $oid _in $iid>] () {
-                test_inner($m, $ow, $or, $iw, $ir);
-            }
-        }
-    };
-    ($m:expr, $ow:expr, $or:expr, $ir:expr, $oid:ident, $iid:ident) => {
-        paste::paste! {
-            expand_tests! { $m, $ow, $or, WritingPolicy::Ignore, $ir, $oid, $iid }
-            expand_tests! { $m, $ow, $or, WritingPolicy::Clog, $ir, $oid, [<Clog $iid>] }
-            expand_tests! { $m, $ow, $or, WritingPolicy::Shutdown, $ir, $oid, [<Shut $iid>] }
-        }
-    };
-    ($m:expr, $ow:expr, $or:expr, $oid:ident, $iid:ident) => {
-        paste::paste! {
-            expand_tests! { $m, $ow, $or, ReadingPolicy::Ignore, $oid, $iid }
-            expand_tests! { $m, $ow, $or, ReadingPolicy::Drain, $oid, [<Drain $iid>] }
-        }
-    };
-    ($m:expr, $or:expr, $oid:ident, $iid:ident) => {
-        paste::paste! {
-            expand_tests! { $m, WritingPolicy::Ignore, $or, $oid, $iid }
-            expand_tests! { $m, WritingPolicy::Clog, $or, [<Clog $oid>], $iid }
-            expand_tests! { $m, WritingPolicy::Shutdown, $or, [<Shut $oid>], $iid }
-        }
-    };
-    ($m:expr, $oid:ident, $iid:ident) => {
-        paste::paste! {
-            expand_tests! { $m, ReadingPolicy::Ignore, $oid, $iid }
-            expand_tests! { $m, ReadingPolicy::Drain, [<Drain $oid>], $iid }
-        }
-    };
-}
-
-expand_tests! { CloseDetectMode::CloseIncomingCheckOutgoing, Check, Close }
-expand_tests! { CloseDetectMode::CloseOutgoingCheckIncoming, Close, Check }
